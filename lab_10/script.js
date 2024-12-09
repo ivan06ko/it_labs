d2b576150586cdbb571a3fdfe79150e5b0962a77
ivan06ko <ivan06komaretskiy@gmail.com>
2024-12-09 09:24:37 +0200

    Delete lab_10 directory

--- lab_10/script.js
+++
@@ -1,9 +0,0 @@
-const descriptions = ["T-42A", "MAUS", "FV215B"];
-
-function showDescription(index) {
-    const descriptionElements = document.querySelectorAll(".description");
-    descriptionElements.forEach((desc, i) => {
-        desc.style.display = i === index && desc.style.display !== "block" ? "block" : "none";
-        if (i === index) desc.textContent = descriptions[index];
-    });
-}
